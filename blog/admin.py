33351cba4de0928213c67c22daf69f586d2f6f73
Kriss-Kross33 <kwamefosu33@gmail.com>
2023-08-22 14:14:04 +0000

    initial commit

---
+++ blog/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    list_filter = ['author', 'publish', 'created', 'status']
+    raw_id_fields = ['author']
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ['title', 'body']
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
